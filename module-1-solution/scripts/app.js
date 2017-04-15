b725c448ac4e721be6671ec5699f8c0a02987f77
Zoltan Toth <contact@zoltantoth.com>
2017-04-15 09:59:50 -0400

    finalized

--- module-1-solution/scripts/app.js
+++ module-1-solution/scripts/app.js
@@ -5,28 +5,30 @@
 	LunchCheckController.$inject = ['$scope'];
 
 	function LunchCheckController($scope) {
-		$scope.list = [];
 		$scope.items = '';
 		$scope.message = '';
 		$scope.className = '';
 
-		$scope.check = function() {
-			if ($scope.items === '') {
-				$scope.list = [];
-				displayMessage(0);
-			} else {
+		$scope.checkQuantity = function() {
+			$scope.list = [];
+			var quantity = 0;
+
+			if ($scope.items !== '') {
 				var dirtyArray = $scope.items.split(','),
 					cleanArray = [];
 
 				for (var i = 0; i < dirtyArray.length; i++) {
-					if (dirtyArray[i] !== '') {
+					if (dirtyArray[i].trim() !== '') {
 						cleanArray.push(dirtyArray[i]);
 					}
 				}
 
 				$scope.list = cleanArray;
-				displayMessage(cleanArray.length);
+				quantity = cleanArray.length;
 			}
+
+			// show message to the user
+			displayMessage(quantity);
 		};
 
 		var displayMessage = function(num) {
