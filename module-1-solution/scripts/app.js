2f72b6236fd8705a52ba71bb23003fb2002e5e0c
Zoltan Toth <contact@zoltantoth.com>
2017-04-14 20:46:05 -0400

    working solution

--- module-1-solution/scripts/app.js
+++ module-1-solution/scripts/app.js
@@ -5,25 +5,40 @@
 	LunchCheckController.$inject = ['$scope'];
 
 	function LunchCheckController($scope) {
+		$scope.list = [];
 		$scope.items = '';
 		$scope.message = '';
+		$scope.className = '';
 
 		$scope.check = function() {
 			if ($scope.items === '') {
+				$scope.list = [];
 				displayMessage(0);
 			} else {
-				$scope.items = $scope.items.split(',').length;
-				displayMessage($scope.items);
+				var dirtyArray = $scope.items.split(','),
+					cleanArray = [];
+
+				for (var i = 0; i < dirtyArray.length; i++) {
+					if (dirtyArray[i] !== '') {
+						cleanArray.push(dirtyArray[i]);
+					}
+				}
+
+				$scope.list = cleanArray;
+				displayMessage(cleanArray.length);
 			}
 		};
 
 		var displayMessage = function(num) {
 			if (num === 0) {
 				$scope.message = 'Please enter data first!';
+				$scope.className = 'danger';
 			} else if (num <= 3) {
 				$scope.message = 'Enjoy!';
+				$scope.className = 'success';
 			} else {
 				$scope.message = 'Too much!';
+				$scope.className = 'success';
 			}
 		};
 	}
